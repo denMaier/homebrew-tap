c610f15ce25ec10eec7b906d2fa5dd7e47cf92c9
Denis Maier <denis@dAir.lab>
2025-09-10 12:04:21 +0200

    the sha doesnt need the prefix

--- Casks/rem.rb
+++ Casks/rem.rb
@@ -2,8 +2,8 @@ cask "rem" do
   arch arm: "arm64", intel: "x64"
 
   version "1.1.0"
-  sha256 arm: "sha256:b717166be47b1955d75b06ab345103be976e63b2c1d113d8f6b620b761370a95",
-         intel: "sha256:73dcd7c913c96378548a2446473d3162f8c036c68f5a2b9720c27ff038f48eb7"
+  sha256 arm: "b717166be47b1955d75b06ab345103be976e63b2c1d113d8f6b620b761370a95",
+         intel: "73dcd7c913c96378548a2446473d3162f8c036c68f5a2b9720c27ff038f48eb7"
     
   url "https://github.com/liriliri/rem/releases/download/v#{version}/REM-#{version}-mac-#{arch}.dmg"
   name "REM"
